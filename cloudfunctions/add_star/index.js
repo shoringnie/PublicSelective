ca3438e4b7ca1713f51533eccca027bab027e27e
ciaoSora <657913055@qq.com>
2019-04-19 21:48:29 +0800

    为界面建立目录
    移动了第三方库的位置，并为每个界面在pages文件夹下建立了子目录

---
+++ cloudfunctions/add_star/index.js
@@ -0,0 +1,59 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  var status = 0
+  var errMsg = "ok"
+  const users = cloud.database().collection("users")
+  var res, user
+  var userDoc = users.doc()
+
+  try {
+    res = await userDoc.get()
+    status = 1
+    user = res.data
+  }
+  catch(e) {
+    console.log(e.lineNumber + "行: " + e.message)
+    errMsg = "add_star: user not existed"
+  }
+  if (status == 0) {
+    return {
+      status: status,
+      errMsg: errMsg,
+    }
+  }
+
+  if (user.stars.some(x => {
+    return x == event.courseid
+  })) {
+    return {
+      status: 0,
+      errMsg: "add_star: course already been stared",
+    }
+  }
+
+  status = 0
+  try {
+    const _ = cloud.database().command
+    res = await userDoc.update({
+      data: {
+        stars: _.push([event.courseid])
+      },
+    })
+    status = 1
+  }
+  catch(e) {
+    console.log(e.lineNumber + "行: " + e.message)
+    errMsg = "add_star: user not existed"
+  }
+
+  return {
+    status: status,
+    errMsg: errMsg,
+  }
+}
